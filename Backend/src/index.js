51e86efe2bdc9e33aee22fb5b8406ec8614064c8
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2025-02-20 15:30:58 +0530

    Projet init

---
+++ Backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+import dotenv from "dotenv"
+const app=express();
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log("App is running at port",PORT)
+})
+
+
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-green-300 ">
+      <h1 className="text-red-500">Hello</h1>
+    </div>
+  )
+}
+
+export default App
