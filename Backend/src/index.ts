c4226e055a9c8d406e28d1c674a069607323f979
akshatg5 <akshatgirdhar5@gmail.com>
2025-02-23 14:35:32 +0530

    feat : db setup

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,13 @@ import express from "express"
 import dotenv from "dotenv"
 const app=express();
 const PORT=3000;
+
+app.use(express.json())
+
+app.get('/',(req,res) => {
+    res.json({"message" : "Hello World"})
+})
+
 app.listen(PORT,()=>{
     console.log("App is running at port",PORT)
 })
